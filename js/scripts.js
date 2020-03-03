97d077cfad2f6208e4dc4062e40026dcb46727a0
Nasra Ibrahim <Nasrinanur@gmail.com>
2020-03-03 10:24:08 +0300

    added changes to html to remove redunduncy

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,56 @@
-$(document).ready(function () {
-    $("form-pizza#form-pizza").submit(function (event) {
-      // event.preventDefault();
-      var name = $("input#pizz1").val();
-      var type = $("input#pizz2").val();
-      var size = $("input#pizz3").val();
-      var toppings = $("input#pizz4").val();
-      var crust = $("input#pizz5").val();
-      var email = $("input#pizz6").val();
-      var message = $("textarea#message").val();
-      function getMessage(){
-        if ($("input#pizz1").val() && $("input#pizz2").val()&& $("input#pizz3").val()&& $("input#pizz4").val()&& $("input#pizz5").val()) {
-            alert("Hi", name + "you've ordered" + type + size + "pizza and chosen" + toppings + "with a" + crust + "crust, please proceed to make payments.");
-          }
-          else {
-            alert("Enter your name and email!");
-          }
-      }
-      
-
-    });
-
-  });
 //calculating pizza price in ksh
-  function calcTotalPrice(quantity, price) {
-    return quantity * price;
-}
-
-function getMessage(quantity, totalPrice) {
-    return 'You purchased ' + quantity + ' pizza(s) and your total price is ksh' + totalPrice
-}
-
-function parseQuantity(val) {
-    return parseInt(val, 10) || 0;
-}
-
-
-function sum(list) {
-    return list.reduce(function(acc, x) {
-        return acc + x;
-    }, 0)
-}
-
-(function() {
+$(document).ready(function () {
+    function calcTotalPrice(quantity, price) {
+        return quantity * price;
+    }
     
-    var submitBtn = document.getElementById('submitButton');
-    var outputPara = document.getElementById('totalPrice');
-
-    var config = [
-        [document.getElementById('quantitySmall'), 500],
-        [document.getElementById('quantityMedium'), 900],
-        [document.getElementById('quantityLarge'), 1200]
-      ];
-
-        submitButton.addEventListener('click', function() {
-            var totalPrices;
-            var quantities = config.map(function (data) {
-                var el = data[0];
-                return parseQuantity(el.value);
-            });
-
-            if ( sum(quantities) > 0 ) {
-                totalPrices = config.map(function(data) {
-                    var el = data[0], price = data[1];
-                    return calcTotalPrice(parseQuantity(el.value), price);
+    function getMessage(quantity, totalPrice) {
+        return 'You purchased ' + quantity + ' pizza(s) and your total price is ksh' + totalPrice
+    }
+    
+    function parseQuantity(val) {
+        return parseInt(val, 10) || 0;
+    }
+    
+    
+    function sum(list) {
+        return list.reduce(function(acc, x) {
+            return acc + x;
+        }, 0)
+    }
+    
+    (function() {
+        
+        var submitBtn = document.getElementById('submitButton');
+        var outputPara = document.getElementById('totalPrice');
+    
+        var config = [
+            [document.getElementById('quantitySmall'), 500],
+            [document.getElementById('quantityMedium'), 900],
+            [document.getElementById('quantityLarge'), 1200]
+          ];
+    
+            submitButton.addEventListener('click', function() {
+                var totalPrices;
+                var quantities = config.map(function (data) {
+                    var el = data[0];
+                    return parseQuantity(el.value);
                 });
+    
+                if ( sum(quantities) > 0 ) {
+                    totalPrices = config.map(function(data) {
+                        var el = data[0], price = data[1];
+                        return calcTotalPrice(parseQuantity(el.value), price);
+                    });
+    
+                    outputPara.innerHTML = getMessage( sum(quantities), sum(totalPrices) );
+                } else {
+                    alert('Please purchase at least 1 pizzaa');
+                }
+            });
+    
+        }());
+    
+  });
 
-                outputPara.innerHTML = getMessage( sum(quantities), sum(totalPrices) );
-            } else {
-                alert('Please purchase at least 1 pizzaa');
-            }
-        });
-
-    }());
+ 
